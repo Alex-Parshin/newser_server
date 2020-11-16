9532b1a1b29de6dff53bde73558417860d1763b4
hramov <trykhramov@gmail.com>
2020-11-16 16:55:38 +0300

    Auto commit 16.11.2020

--- controllers/rabbitmq/index.js
+++ controllers/rabbitmq/index.js
@@ -5,9 +5,8 @@ import { log } from './../../logger'
 import _ from 'underscore'
 
 export async function getData(queue) {
-    let data = null
     return await new Promise((resolve, reject) => {
-        data = amqp.connect(`amqp://${process.env.RABBIT_USER}:${process.env.RABBIT_PASSWORD}@${process.env.RABBIT_HOST}:${process.env.RABBIT_PORT}`,
+        amqp.connect(`amqp://${process.env.RABBIT_USER}:${process.env.RABBIT_PASSWORD}@${process.env.RABBIT_HOST}:${process.env.RABBIT_PORT}`,
             function(error0, connection) {
                 if (error0) {
                     throw error0;
@@ -35,12 +34,6 @@ export async function getData(queue) {
 
 export async function sendData(queue, data) {
     let status = null
-    let sliced_array = []
-
-    if (data.length > 50) {
-        sliced_array = _.chunk(data, 50);
-    }
-
     try {
         status = await new Promise(async(resolve, reject) => {
             amqp.connect(`amqp://${process.env.RABBIT_USER}:${process.env.RABBIT_PASSWORD}@${process.env.RABBIT_HOST}:${process.env.RABBIT_PORT}`,
@@ -49,7 +42,7 @@ export async function sendData(queue, data) {
                         reject(error0)
                         return
                     }
-                    log('Подключился к RabbitMQ')
+                    log(`Подключился к RabbitMQ ${process.env.RABBIT_USER}`)
                     connection.createChannel(function(error1, channel) {
                         if (error1) {
                             reject(error1)
@@ -60,16 +53,17 @@ export async function sendData(queue, data) {
                         });
                         channel.prefetch(1);
 
-                        for (let i = 0; i < sliced_array.length; i++) {
-                            log('Отправляю порции данных')
-                            channel.sendToQueue(queue,
-                                Buffer.from(JSON.stringify(sliced_array[i])), {
-                                    headers: {
-                                        time: new Date(Date.now()).toLocaleString()
-                                    }
-                                });
-                        }
-                        log('Отправил данные ')
+                        log('Отправляю порции данных')
+                        channel.sendToQueue(queue,
+                            Buffer.from(JSON.stringify(data)), {
+                                headers: {
+                                    time: new Date(Date.now()).toLocaleString()
+                                }
+                            });
+
+                        log(`Отправил ${data.length} новостей`)
+                        connection.close()
+                        log('Закрыл соединение')
                         resolve(true);
                     });
                 })
